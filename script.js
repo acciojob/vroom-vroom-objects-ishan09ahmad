bf5bca6e501b4cab6a5fc5e7df8e1353cb3ca18c
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-03-27 02:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+car.prototype.getMakeModel=function () {
+	return `"${this.make} ${this.model}"`
+}
+function SportsCar(make, model, topSpeed) {
+	car.call(this,make,model);
+}
 
+SportsCar.prototype = Object.create(Car.prototype);
+
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
