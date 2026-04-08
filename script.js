a2bff81aa8db25cf68c155e8d33bdaf74f19db79
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-09 02:05:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+  this.make = make;
+  this.model = model;
 }
 
-Car.prototype.getMakeModel=function () {
-	return `"${this.make} ${this.model}"`
-}
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
+
 function SportsCar(make, model, topSpeed) {
-	car.call(this,make,model);
-	this.topSpeed=topSpeed;
+  Car.call(this, make, model); // FIXED
+  this.topSpeed = topSpeed;
 }
+
 
 SportsCar.prototype = Object.create(Car.prototype);
-
 SportsCar.prototype.constructor = SportsCar;
 
 SportsCar.prototype.getTopSpeed = function () {
@@ -23,13 +23,9 @@ SportsCar.prototype.getTopSpeed = function () {
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed());  
 
-console.log(car.getMakeModel());
-
-
-console.log(car.getTopSpeed()); 
-
-
-// Do not change the code below
+// Do not change
 window.Car = Car;
 window.SportsCar = SportsCar;
