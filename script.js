6ff733550de6e1c703d6b2c6629dad1a7ee71b6c
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-09 01:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ SportsCar.prototype.constructor = SportsCar;
 SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
+
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+
+console.log(car.getMakeModel());
+
+
+console.log(car.getTopSpeed()); 
+
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
